f603895a48862fc88620899786775d6280133297
Chris Jones <chris.jones@jivesoftware.com>
2015-05-05 16:41:12 -0700

    wip

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject clj-intro "0.0.1"
   :url "http://github.com/chrisfjones/clj-intro"
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.apache.commons/commons-math3 "3.5"]]
   :plugins [])
--- src/clj/clj_intro1.clj
+++ src/clj/clj_intro1.clj
@@ -1,3 +1,35 @@
-(ns clj-intro1)
+; Pre
+; 1. I've been working with clojure for a few years on small/medium size projects
+; 2. I'll draw a lot of comparisons with Java, but I don't mean to put it down
+; 3. "Clojure asks you to step up your game, and rewards you for doing so" - Stuart Halloway
+; 4. I struggled a bit with the flow of this material. There are several concepts that all fit together to form the whole, but there is no clear sequence.
 
-(println "hey")
+
+; 1. Live coding
+(+ 1 1)
+
+
+; 2. Java interop
+(System/currentTimeMillis)
+
+(org.apache.commons.math3.util.ArithmeticUtils/addAndCheck 1 1)
+
+; 3. Data as Code
+
+
+
+; 4. Code as Data
+
+
+
+; 5. Immutability
+
+
+
+
+; 6. atoms/refs
+
+
+
+
+; sidebar - efficiency, show pic of node tree for clj list impl
